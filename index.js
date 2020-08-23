bfbdde95d16df3175ec4bc460f0ffb402dd2f1ee
aaroncaraway <aaroncaraway42@gmail.com>
2020-08-23 14:48:59 -0700

    S21 234, complete

--- index.js
+++ index.js
@@ -5,13 +5,27 @@ const fetchData = async (searchTerm) => {
       s: searchTerm,
     },
   });
-  console.log(response.data);
+
+  if (response.data.Error) {
+    return [];
+  }
+  return response.data.Search;
 };
 
 const input = document.querySelector("input");
 
-const sendInput = (event) => {
-  fetchData(event.target.value);
+const sendInput = async (event) => {
+  const movies = await fetchData(event.target.value);
+  console.log(movies);
+  for (let movie of movies) {
+    const div = document.createElement("div");
+
+    div.innerHTML = `
+      <img src="${movie.Poster}"/>
+      <h2>${movie.Title}</h2>
+    `;
+    document.querySelector("#target").appendChild(div);
+  }
 };
 
 input.addEventListener("input", debounce(sendInput, 500));
